3459f1ca31adaaec063df999bde616af75c1eb16
Jose Luis Herrera <jose-luis1997@hotmail.com>
2022-02-12 16:19:13 -0700

    Added first name, last name to devise signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_12_221643) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_12_223746) do
   create_table "books", force: :cascade do |t|
     t.integer "book_id"
     t.string "title"
@@ -34,6 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_12_221643) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
