867d841a702bd6c6bc5e479e0968fa1bd94c8bbd
Jose Luis Herrera <jose-luis1997@hotmail.com>
2022-02-12 16:41:09 -0700

    Added balance to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_12_232209) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_12_232929) do
   create_table "books", force: :cascade do |t|
     t.integer "book_id"
     t.string "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_12_232209) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
+    t.float "balance", default: 50.0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
