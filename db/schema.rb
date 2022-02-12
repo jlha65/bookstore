b9f31ee1ce1a3d792ed9e900e17024e3d5f62433
Jose Luis Herrera <jose-luis1997@hotmail.com>
2022-02-12 16:26:03 -0700

    Added address field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_12_223746) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_12_232209) do
   create_table "books", force: :cascade do |t|
     t.integer "book_id"
     t.string "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_12_223746) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
